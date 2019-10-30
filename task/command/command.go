ecbdfa820e287ec01cf14aa87b2de1f19bb94752
craiggwilson <craiggwilson@gmail.com>
2019-10-30 09:32:53 -0500

    fixed old command.Command package to use new sh package.

--- task/command/command.go
+++ task/command/command.go
@@ -2,21 +2,22 @@ package command
 
 import (
 	"os/exec"
-	"strings"
 
+	"github.com/craiggwilson/goke/pkg/sh"
 	"github.com/craiggwilson/goke/task"
 )
 
 // Command wraps exec.Command in a task executor.
 func Command(name string, args ...string) task.Executor {
-	return Executor(exec.Command(name, args...))
+	return func(ctx *task.Context) error {
+		return sh.Run(ctx, name, args...)
+	}
 }
 
 // Executor creates a task.Executor from the command.
 func Executor(cmd *exec.Cmd) task.Executor {
 	return func(ctx *task.Context) error {
-		ctx.Logf("exec: '%s %s'\n", cmd.Path, strings.Join(cmd.Args[1:], " "))
-
+		sh.LogCmd(ctx, cmd)
 		return cmd.Run()
 	}
 }
